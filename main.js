37bc3f04c8a6a777257f3a2e1eb36dccfcafdda2
Justin Boyer <justinbboyer@gmail.com>
2021-08-10 13:02:37 -0700

    add adress and gps queries

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 const key = process.env.BING_KEY;
+const baseURL = `http://dev.virtualearth.net/REST/V1/Routes/Walking`;
+const options = `&optmz=distance&output=xml&key=${key}`
 
 app.get('/', cors(), function(req, res) {
     const port = process.env.PORT || 3000;
@@ -15,9 +17,7 @@ app.get('/', cors(), function(req, res) {
 });
 
 app.get('/test', cors(), function(req, res) {
-    const baseURL = `http://dev.virtualearth.net/REST/V1/Routes/Walking`;
     const path = `?wp.0=Eiffel%20Tower&wp.1=louvre%20museum`;
-    const options = `&optmz=distance&output=xml&key=${key}`;
     const url = baseURL + path + options;
     axios.get(url).then(response => {
         console.log(response.status);
@@ -27,8 +27,20 @@ app.get('/test', cors(), function(req, res) {
 
 });
 
+app.get('/gps/:startlat/:startlong/:endlat/:endlong', cors(), function(req, res) {
+    const startWaypoint = `?wp.0=${req.params.startlat},${req.params.startlong}`
+    const endstartWaypoint = `&wp.1=${req.params.endlat},${req.params.endlong}`;;
+    const url = baseURL + startWaypoint + endstartWaypoint + options;
+    axios.get(url).then(response => {
+        console.log(response.status);
+        res.writeHead(200, { 'Content-Type': 'text' });
+        res.end(JSON.stringify(response.data));
+    });
+});
+
 app.get('/address/:start/:end', cors(), function(req, res) {
-    url = `http://dev.virtualearth.net/REST/V1/Routes/Walking?wp.0=Eiffel%20Tower&wp.1=louvre%20museum&optmz=distance&output=xml&key=${key}`
+    const path = `?wp.0=${req.params.start}&wp.1=${req.params.end}`;
+    const url = baseURL + path + options;
     axios.get(url).then(response => {
         console.log(response.status);
         res.writeHead(200, { 'Content-Type': 'text' });
