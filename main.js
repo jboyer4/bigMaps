d0311314a2608b9e20b30a13c1abc9658b054a30
Justin Boyer <justinbboyer@gmail.com>
2021-08-10 11:57:00 -0700

    add env

---
+++ main.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const axios = require('axios');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+const key = process.env.BING_KEY;
+
+app.get('/path', cors(), function(req, res) {
+    const port = process.env.PORT || 3000;
+    url = `http://dev.virtualearth.net/REST/V1/Routes/Walking?wp.0=Eiffel%20Tower&wp.1=louvre%20museum&optmz=distance&output=xml&key=${key}`
+    axios.get(url).then(response => {
+        console.log(response.status);
+        res.writeHead(200, { 'Content-Type': 'text' });
+        res.end(JSON.stringify(response.data));
+    });
+
+})
+
+app.listen(port, "0.0.0.0", function() {
+    console.log("Listening on Port 3000");
+});
+console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
